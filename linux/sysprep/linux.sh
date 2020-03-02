d6d9f7a8c426c85d0339f3edf38a39be458132c8
hybridadmin <46610714+hybridadmin@users.noreply.github.com>
2020-03-02 17:49:46 +0200

    Update linux.sh

--- linux/sysprep/linux.sh
+++ linux/sysprep/linux.sh
@@ -86,14 +86,19 @@ function harden_ssh(){
     MACS="hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha2-256,umac-128-etm@openssh.com"
     CIPHERS="chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com"
     
-    if [ $OS_NAME == "centos" ] && [ $OS_RELEASE -ge 8 ]; then sed -i '/^# CRYPTO_POLICY=.*/s/^#//g' /etc/sysconfig/sshd; fi 
-    
     if [ $OS_NAME == "ubuntu" ] && [ $OS_RELEASE -ge 16 ]; then
         sed -i 's/#\(.*ssh_host.*\(rsa\|ed25519\).*\)/\1/' /etc/ssh/sshd_config
     else
         sed -i 's/^HostKey \/etc\/ssh\/ssh_host_\(dsa\|ecdsa\)_key$/\#HostKey \/etc\/ssh\/ssh_host_\1_key/g' /etc/ssh/sshd_config       
     fi    
-    echo -e "\n\n# Restricted key exchange, cipher, and MAC algorithms \nKexAlgorithms ${KEXALGS} \nCiphers ${CIPHERS} \nMACs ${MACS}" >> /etc/ssh/sshd_config
+    
+    if [ $OS_NAME == "centos" ] && [ $OS_RELEASE -ge 8 ]
+    	sed -i 's/^[^#]*CRYPTO_POLICY/#&/' /etc/sysconfig/sshd
+	cp /etc/crypto-policies/back-ends/opensshserver.config /etc/crypto-policies/back-ends/opensshserver.config.orig
+	echo -e "CRYPTO_POLICY='-oCiphers=chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr -oMACs=hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com -oGSSAPIKexAlgorithms=gss-curve25519-sha256- -oKexAlgorithms=curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256 -oHostKeyAlgorithms=ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512,ssh-rsa,ssh-rsa-cert-v01@openssh.com -oPubkeyAcceptedKeyTypes=ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512,ssh-rsa,ssh-rsa-cert-v01@openssh.com'" > /etc/crypto-policies/back-ends/opensshserver.config
+    else
+    	echo -e "\n\n# Restricted key exchange, cipher, and MAC algorithms \nKexAlgorithms ${KEXALGS} \nCiphers ${CIPHERS} \nMACs ${MACS}" >> /etc/ssh/sshd_config
+    fi
    
     rm /etc/ssh/ssh_host_*key*
     ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ""
